79f5ced9abc3b4d9c5394cae9ccd6cea64a86fda
silentgraphic <kierannolan1995@gmail.com>
2024-09-11 13:53:03 +0100

    Added in onload render of quotes

--- quote/src/App.js
+++ quote/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { updateQuote } from './features/quote';
 
 function App() {
   const quoteSlice = useSelector((state) => state.quote);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(updateQuote());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <p>Quote: {quoteSlice.quote}</p>
