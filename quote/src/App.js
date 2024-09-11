ffcca36b0183dc0354567328023c7a2555856bdb
silentgraphic <kierannolan1995@gmail.com>
2024-09-11 10:27:04 +0100

    Added author

--- quote/src/App.js
+++ quote/src/App.js
@@ -3,11 +3,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { updateQuote } from './features/quote';
 
 function App() {
-  const quoteSlice = useSelector((state) => state.quote.quote);
+  const quoteSlice = useSelector((state) => state.quote);
   const dispatch = useDispatch();
   return (
     <div className="App">
-      <p>Quote: {quoteSlice}</p>
+      <p>Quote: {quoteSlice.quote}</p>
+      <p>by: {quoteSlice.author}</p>
       <button onClick={() => dispatch(updateQuote())}>Get quote</button>
     </div>
   );
--- quote/src/features/quote.js
+++ quote/src/features/quote.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-    quote: ""
+    quote: "",
+    author: ""
 };
 
 export const updateQuote = createAsyncThunk("quote/updateQuote", async () => {
@@ -19,9 +20,11 @@ export const quoteSlice = createSlice({
         builder
             .addCase(updateQuote.pending, (state, action) => {
                 state.quote = "Loading";
+                state.author = "Loading";
             })
             .addCase(updateQuote.fulfilled, (state, action) => {
                 state.quote = action.payload["0"].q;
+                state.author = action.payload["0"].a;
             })
             .addCase(updateQuote.rejected, (state, action) => {
                 state.quote = "Error has occured";
