9caa92af0b74b9314108b3d64e1b1386e25d2976
silentgraphic <kierannolan1995@gmail.com>
2024-09-13 08:37:30 +0100

    Added error handling

--- quote/src/features/quote.js
+++ quote/src/features/quote.js
@@ -23,9 +23,15 @@ export const quoteSlice = createSlice({
                 state.status = "Loading";
             })
             .addCase(updateQuote.fulfilled, (state, action) => {
-                state.quote = action.payload["0"].q;
-                state.author = action.payload["0"].a;
-                state.status = "Fulfilled";
+                console.log(action.payload["0"].q);
+                if (action.payload["0"].q === "Too many requests. Obtain an auth key for unlimited access.") {
+                    state.quote = "Slow down! You have sent too many requests!";
+                    state.status = "Error";
+                } else {
+                    state.quote = action.payload["0"].q;
+                    state.author = action.payload["0"].a;
+                    state.status = "Fulfilled";
+                }
             })
             .addCase(updateQuote.rejected, (state, action) => {
                 state.quote = "Error has occured";
